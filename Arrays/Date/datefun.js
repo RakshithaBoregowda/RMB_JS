e986111b8bc2d26c64697aa07954411a2459b087
RakshithaBoregowda <rakshithab9718@gmail.com>
2022-10-03 11:27:09 +0530

    changes commited

---
+++ Arrays/Date/datefun.js
@@ -0,0 +1,7 @@
+var d=new Date()
+
+console.log(d.toDateString())
+console.log("today's date :"+d.getDate())
+console.log("today's day :"+d.getDay())
+console.log("today's year :"+d.getFullYear())
+console.log("today's month :"+d.getMonth())
